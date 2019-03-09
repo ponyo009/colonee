5b4c4da6085a16e8af252f1b46895a4ae4c7e1a0
flowerriver <ka.0905.hanahana@gmail.com>
2019-03-09 11:28:33 +0900

    Initial Commit

---
+++ gamies.comTests/gamies_comTests.swift
@@ -0,0 +1,34 @@
+//
+//  gamies_comTests.swift
+//  gamies.comTests
+//
+//  Created by hanakawa kazuya on 2019/03/09.
+//  Copyright © 2019 hanakawa kazuya. All rights reserved.
+//
+
+import XCTest
+@testable import gamies_com
+
+class gamies_comTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
