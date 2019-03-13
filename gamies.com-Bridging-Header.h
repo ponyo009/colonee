465e06c4457b1d9a001c40299fbfdd91cfe95493
flowerriver <ka.0905.hanahana@gmail.com>
2019-03-13 11:42:30 +0900

    make gemfile

---
+++ gamies.com-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  gamies.com-Bridging-Header.h
+//  gamies.com
+//
+//  Created by hanakawa kazuya on 2019/03/13.
+//  Copyright © 2019 hanakawa kazuya. All rights reserved.
+//
+
+#ifndef gamies_com_Bridging_Header_h
+#define gamies_com_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+#import "gamies.comKeys.h"
+#endif /* gamies_com_Bridging_Header_h */
