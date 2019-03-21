91ec60570c6ae856a81669620d14b8e4e9475032
flowerriver <ka.0905.hanahana@gmail.com>
2019-03-21 18:19:30 +0900

    termsofuser

---
+++ gamies.com/TermesOfUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TermesOfUserViewController.swift
+//  Bolts
+//
+//  Created by hanakawa kazuya on 2019/03/21.
+//
+
+import UIKit
+
+class TermesOfUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ gamies.com/TermsOfUsersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TermsOfUsersViewController.swift
+//  gamies.com
+//
+//  Created by hanakawa kazuya on 2019/03/21.
+//  Copyright © 2019 hanakawa kazuya. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfUsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
