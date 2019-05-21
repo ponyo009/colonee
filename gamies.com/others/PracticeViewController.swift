02a6ad18114815fec175211f78723b51d1e26402
Akira Norose <a.anyanyan@outlook.com>
2019-05-21 16:04:49 +0900

    ChoodeGame

--- gamies.com/others/PracticeViewController.swift
+++ gamies.com/others/PracticeViewController.swift
@@ -30,6 +30,8 @@ extension Array where Element: Equatable {
 
 class PracticeViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let dispatchGroup = DispatchGroup()
     let dispatchQueue = DispatchQueue(label: "queue", attributes: .concurrent)
 
@@ -42,7 +44,10 @@ class PracticeViewController: UIViewController {
     var userGameNames = [String]()
     var diffGameIDs = [String]()
     var diffGameNames = [String]()
-
+    var gameForCells = [DictionaryLiteral<Any, Any>]()
+    
+    let cellidentifier = "choosegamecell"
+    var gameicon: UIImageView!
     
     
     func fetchAllgames () {
@@ -51,13 +56,13 @@ class PracticeViewController: UIViewController {
         dispatchQueue.async(group: dispatchGroup) {
             [weak self] in
             ref.getDocuments { (documents, err) in
-                print("start1")
+                //print("start1")
                 let documents = documents?.documents
                 for document in documents! {
                     self!.gameIDs.append(document.documentID)
                     self!.gameNames.append(document.data()["title"] as! String)
                 }
-                print("finished1")
+                //print("finished1")
                 self!.dispatchGroup.leave()
             }
         }
@@ -69,13 +74,14 @@ class PracticeViewController: UIViewController {
         dispatchQueue.async(group: dispatchGroup) {
             [weak self] in
             ref.getDocuments { (documents, err) in
-                print("start2")
+                //print("start2")
                 let documents = documents?.documents
                 for document in documents! {
                     self!.userGameNames.append(document.data()["title"] as! String)
                     self!.userGameIDs.append(document.documentID)
+                    self!.gameForCells.append([document.documentID: 1])
                 }
-                print("finished2")
+                //print("finished2")
                 self!.dispatchGroup.leave()
             }
         }
@@ -84,13 +90,18 @@ class PracticeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         fetchAllgames()
         fetchUserGames()
         
         dispatchGroup.notify(queue: .main){
-            print(self.gameNames.subtracting(self.userGameNames))
-            
+            self.diffGameIDs = self.gameIDs.subtracting(self.userGameIDs)
+            self.diffGameNames = self.gameNames.subtracting(self.userGameNames)
+            for game in self.diffGameIDs {
+                self.gameForCells.append([game: 2])
+            }
+            self.collectionView.dataSource = self
+            self.collectionView.delegate = self
+            self.collectionView.reloadData()
         }
         
         
@@ -108,4 +119,29 @@ class PracticeViewController: UIViewController {
     }
     */
 
+}
+
+extension PracticeViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return gameIDs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellidentifier, for: indexPath)
+        let gameForCell = gameForCells[indexPath.row]
+        let gameId = gameForCell[0].key as! String
+        let gameTag = gameForCell[0].value as! Int
+       
+        gameicon = cell.viewWithTag(1) as? UIImageView
+        gameicon.image = UIImage(named: gameId)
+        cell.tag = gameTag
+        
+        return cell
+    }
+}
+
+extension PracticeViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //
+    }
 }
