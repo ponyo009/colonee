278bc1ae4ce0135acd09714fe4e543b02ba450a3
Akira Norose <a.anyanyan@outlook.com>
2019-07-27 12:05:38 +0900

    20190727

---
+++ gamies.com/Chat/AllTalkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AllTalkViewController.swift
+//  gamies.com
+//
+//  Created by akira on 2019/07/20.
+//  Copyright © 2019 hanakawa kazuya. All rights reserved.
+//
+
+import UIKit
+
+class AllTalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
